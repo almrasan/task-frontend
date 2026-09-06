1a1fdf0e6bd3e82c4d24d1c9df646a63ac402019
rasan <rasanalm6446@gmail.com>
2026-09-06 09:57:37 +0530

    Add mobile responsive styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
